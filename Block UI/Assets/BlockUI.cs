09570f054672ab067b9226253c5e1c72982c147d
Tom Gerke <tomgerke10@gmail.com>
2017-07-16 10:53:10 -0400

    Block Prefab and Text Mesh Pro, started scripts
    installed textmesh pro assets, changed text gamobjects over to TMP, began scrips for instantiating A-Z blocks

---
+++ Block UI/Assets/BlockUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockUI : MonoBehaviour {
+
+    public GameObject[] blocks;
+    public GameObject blockPrefab;
+
+    private BlockUI blockToAdd;
+
+
+	// Use this for initialization
+	void Start () {
+
+        spawnBlocks();
+
+    }
+
+    void spawnBlocks()
+    {
+        
+        for (char letter = 'A'; letter <= 'Z'; letter++)
+        {
+            blockToAdd = GameObject.Instantiate(blockPrefab).GetComponent<Card>(); //switch Card with block, figure out how to change text
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
